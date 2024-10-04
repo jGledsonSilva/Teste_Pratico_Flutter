7a8e6f3fcb9bfd552b1a8c1afdf0ac847d3fefb9
jGledsonSilva <gledson.silva60@aluno.ifce.edu.br>
2024-10-04 17:36:13 -0300

    criando entidade

---
+++ listagem_de_itens/lib/entities/item_entity.dart
@@ -0,0 +1,13 @@
+class ItemEntity {
+  final String id;
+  final String image;
+  final String title;
+  final String description;
+
+  ItemEntity({
+    required this.id,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
